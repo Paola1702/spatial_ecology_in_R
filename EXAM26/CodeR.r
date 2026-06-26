f0d651c3b89a22a7d4c13639a3c17b03535033e5
Paola Marletto <paola.marletto@studio.unibo.it>
2026-06-26 16:37:41 +0100

    Update and rename EXAM to EXAM26/CodeR.r

---
+++ EXAM26/CodeR.r
@@ -0,0 +1,4 @@
+
+# hypothesis: damage on the forest vegetation in Portugal after severe storms 
+#https://riskandrecovery.emergency.copernicus.eu/EMSN233/reporting/
+#Parque Natural das Serras de Aire e Candeeiros
