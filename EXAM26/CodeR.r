973e2f872d8031e32fc9282378c1d946f638e621
Paola Marletto <paola.marletto@studio.unibo.it>
2026-06-26 17:42:15 +0100

    Update CodeR.r

--- EXAM26/CodeR.r
+++ EXAM26/CodeR.r
@@ -1,4 +1,6 @@
 
 # hypothesis: damage on the forest vegetation in Portugal after severe storms 
 #https://riskandrecovery.emergency.copernicus.eu/EMSN233/reporting/
+#https://www.icnf.pt/conservacao/rnapareasprotegidas/parquesnaturais/pnserrasdeaireecandeeiros
+#https://snig.dgterritorio.gov.pt/rndg/srv/search?format=Esri%20Shapefile
 #Parque Natural das Serras de Aire e Candeeiros
