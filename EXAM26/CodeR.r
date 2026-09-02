3b6010caa879f0d01737ebeeb4b3df0ba6135fd3
Paola Marletto <paola.marletto@studio.unibo.it>
2026-09-02 16:26:17 +0200

    Update CodeR.r

--- EXAM26/CodeR.r
+++ EXAM26/CodeR.r
@@ -1,8 +1,9 @@
 #Pre-Vaia: estate 2018 (prima del 30 ottobre) — bosco integro
 #Post-Vaia, pre-bostrico: estate 2019 — danno da schianto già visibile (chiome a terra, radure nette)
-#Fase epidemica bostrico: estate 2022 o 2023 — ulteriore perdita di chioma nelle piante rimaste in piedi, prima dei tagli sanitari
-##############################################################################
 
+##############################################################################
+#IMPACT OF THE VAIA STORM (30 OCTOBER 2018) ON THE VEGETATION OF THE PARCO NATURALE PANEVEGGIO - PALE DI SAN MARTINO
+# Analysis of the Sentinel-2 images (pre-Vaia 2018, post-vaia same season 2019)
 ##############################################################################
 
 # LIBRARY AND WORKING DIRECTORY
